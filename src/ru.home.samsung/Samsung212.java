91d22d1f2d851e7ba0bedfb7961f4ea058eb4013
anayavolapats <anay.avolapats@mail.ru>
2019-11-24 17:05:48 +0300

    fignya

--- src/ru.home.samsung/Samsung212.java
+++ src/ru.home.samsung/Samsung212.java
@@ -9,13 +9,13 @@ public class Samsung212 {
         System.out.println(Integer.toBinaryString(i));
         System.out.println(Integer.toOctalString(i));
         System.out.println(Integer.toHexString(i));
-        if(i >= 0 && i <= 255){
+        if(i >= 0 && i <= 2565){
             System.out.println("YES");
         }
         else {
-            System.out.println("NO");
+            System.out.println("NO!!!");
         }
-        if(i >= -32768 && i <= 32767){
+        if(i >= -32768 && i <= 327343467){
             System.out.println("YES");
         }
         else {
