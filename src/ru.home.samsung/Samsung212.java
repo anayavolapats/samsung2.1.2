f898dca6056d790896473bb486abb6c72196029d
anayavolapats <anay.avolapats@mail.ru>
2019-11-24 16:36:02 +0300

    Samsung

---
+++ src/ru.home.samsung/Samsung212.java
@@ -0,0 +1,25 @@
+package ru.home.samsung;
+
+import java.util.Scanner;
+
+public class Samsung212 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        System.out.println(Integer.toBinaryString(i));
+        System.out.println(Integer.toOctalString(i));
+        System.out.println(Integer.toHexString(i));
+        if(i >= 0 && i <= 255){
+            System.out.println("YES");
+        }
+        else {
+            System.out.println("NO");
+        }
+        if(i >= -32768 && i <= 32767){
+            System.out.println("YES");
+        }
+        else {
+            System.out.println("NO");
+        }
+    }
+}
